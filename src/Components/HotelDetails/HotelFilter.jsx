17df93790850272d01738f08b2d3bfe46a2e485e
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-12-29 21:25:48 +0530

    Refactor hotel filter and nested routing components

--- src/Components/HotelDetails/HotelFilter.jsx
+++ src/Components/HotelDetails/HotelFilter.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
 
 function HotelFilter() {
-    const location = useLocation();
-    const isActive = (path) => location.pathname === path;
-    const navItemStyle = {
-        backgroundColor: '#edf2f7',
-        padding: '12px 18px',
-        borderRadius: '8px',
-        fontSize: '15px',
-        fontWeight: '600',
-        marginBottom: '10px', // Adjust as needed for spacing
-        cursor: 'pointer',
-        textAlign: 'center',
-    };
+  const location = useLocation();
+  const { id } = useParams();
+  const isActive = (path) => location.pathname === path;
 
-    return (
-        <div className='grid grid-cols-3 md:grid-cols-6 gap-4 px-2 md:px-0'>
-            <Link to="/hoteldetails/" className={`no-underline  ${isActive('/hoteldetails/') ? "text-[#f7484d]" : 'text-slate-500'}`} >
-                <div style={navItemStyle}>Room</div>
-            </Link>
-            <Link to="/hoteldetails/animities" className={`no-underline ${isActive('/hoteldetails/animities') ? "text-[#f7484d]" : 'text-slate-500'}`}>
-                <div className='nav-item' style={navItemStyle}>Amenities</div>
-            </Link>
-            <Link to="animities/discription" className={`no-underline   ${isActive('/hoteldetails/animities/discription') ? "text-[#f7484d]" : 'text-slate-500'}`}>
-                <div style={navItemStyle}>Description</div>
-            </Link>
-            <Link to="animities/discription/review" className={`no-underline  ${isActive('/hoteldetails/animities/discription/review') ? "text-[#f7484d]" : 'text-slate-500'}`}>
-                <div style={navItemStyle}>Review</div>
-            </Link>
-            <Link to="animities/discription/review/support" className={`no-underline  ${isActive('/hoteldetails/animities/discription/review/support') ? "text-[#f7484d]" : 'text-slate-500'}`}>
-                <div style={navItemStyle}>Support</div>
-            </Link>
-            <Link to="animities/discription/review/support/photos/" className={`no-underline  ${isActive('/hoteldetails/animities/discription/review/support/photos/') ? "text-[#f7484d]" : 'text-slate-500'}`}>
-                <div style={navItemStyle}>Photos</div>
-            </Link>
-        </div>
-    )
+  const navItemStyle = {
+    backgroundColor: '#edf2f7',
+    padding: '12px 18px',
+    borderRadius: '8px',
+    fontSize: '15px',
+    fontWeight: '600',
+    marginBottom: '10px',
+    cursor: 'pointer',
+    textAlign: 'center',
+  };
+
+  return (
+    <div className='grid grid-cols-3 md:grid-cols-6 gap-4 px-2 md:px-0'>
+      <Link to={`/hoteldetails/${id}/rooms`} className={`no-underline ${isActive(`/hoteldetails/${id}/rooms`) ? 'text-[#f7484d]' : 'text-slate-500'}`}>
+        <div style={navItemStyle}>Room</div>
+      </Link>
+      <Link to={`/hoteldetails/${id}/amenities`} className={`no-underline ${isActive(`/hoteldetails/${id}/amenities`) ? 'text-[#f7484d]' : 'text-slate-500'}`}>
+        <div className='nav-item' style={navItemStyle}>Amenities</div>
+      </Link>
+      <Link to={`/hoteldetails/${id}/description`} className={`no-underline ${isActive(`/hoteldetails/${id}/description`) ? 'text-[#f7484d]' : 'text-slate-500'}`}>
+        <div style={navItemStyle}>Description</div>
+      </Link>
+      <Link to={`/hoteldetails/${id}/review`} className={`no-underline ${isActive(`/hoteldetails/${id}/review`) ? 'text-[#f7484d]' : 'text-slate-500'}`}>
+        <div style={navItemStyle}>Review</div>
+      </Link>
+      <Link to={`/hoteldetails/${id}/support`} className={`no-underline ${isActive(`/hoteldetails/${id}/support`) ? 'text-[#f7484d]' : 'text-slate-500'}`}>
+        <div style={navItemStyle}>Support</div>
+      </Link>
+      <Link to={`/hoteldetails/${id}/photos`} className={`no-underline ${isActive(`/hoteldetails/${id}/photos`) ? 'text-[#f7484d]' : 'text-slate-500'}`}>
+        <div style={navItemStyle}>Photos</div>
+      </Link>
+    </div>
+  );
 }
 
-export default HotelFilter
+export default HotelFilter;
--- src/Components/HotelDetails/NestedRouting.jsx
+++ src/Components/HotelDetails/NestedRouting.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import HotelRooms from './HotelRooms'
-import HotelAnimites from './HotelAnimites'
-import HotelDiscription from './HotelDiscription'
-import HotelReviews from './HotelReviews'
-import HotelSupport from './HotelSupport'
-import HotelPhotos from './HotelPhotos'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HotelRooms from './HotelRooms';
+import HotelAnimites from './HotelAnimites';
+import HotelDiscription from './HotelDiscription';
+import HotelReviews from './HotelReviews';
+import HotelSupport from './HotelSupport';
+import HotelPhotos from './HotelPhotos';
 
 function NestedRouting() {
-    return (
-        <Routes>
-            <Route path='/' element={<HotelRooms />} />
-            <Route path='animities' element={<HotelAnimites />} />
-            <Route path='animities/discription' element={<HotelDiscription />} />
-            <Route path='animities/discription/review' element={<HotelReviews />} />
-            <Route path='animities/discription/review/support' element={<HotelSupport />} />
-            <Route path='animities/discription/review/support/photos/*' element={<HotelPhotos />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path='/:id' element={<HotelRooms />} />
+      <Route path='/:id/amenities' element={<HotelAnimites />} />
+      <Route path='/:id/description' element={<HotelDiscription />} />
+      <Route path='/:id/review' element={<HotelReviews />} />
+      <Route path='/:id/support' element={<HotelSupport />} />
+      <Route path='/:id/photos/' element={<HotelPhotos />} />
+    </Routes>
+  );
 }
 
-export default NestedRouting
+export default NestedRouting;
