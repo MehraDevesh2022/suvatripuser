8d4ce9c1d10c77dbba620e89fe5f28a97c6937c2
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-12-29 14:29:14 +0530

    update

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -6,42 +6,53 @@ import { FaUser } from 'react-icons/fa6';
 import axios from 'axios';
 import SignUp from './SignUp/SignUp';
 import ProfileDropdown from './SignUp/ProfileDropdown';
-
+import { useAppContext } from '../../context/store';
 function Navbar() {
   const [showSignup, setShowSignup] = useState(false);
   const [showProfileDropdown, setShowProfileDropdown] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { state, actions } = useAppContext();
 
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      const token = localStorage.getItem('token');
-
-      if (token) {
-        try {
-          const response = await axios.get('http://localhost:8000/auth/profile', {
-            headers: { Authorization: `Bearer ${token}` },
-          });
-
-          if (response.data) {
-            setIsLoggedIn(true);
-          } else {
-            setIsLoggedIn(false);
-          }
-        } catch (error) {
-          setIsLoggedIn(false);
-        }
+  const personalProfile = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.error("Token is missing");
+        return;
       }
-    };
+    
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+       
+      };
+  
+      const response = await axios.get("http://localhost:8000/auth/profile", config);
+  
+  
+      if (response.data.success && response.data.success === true) {
+          console.log(response.data.user , "response.data.user");
+        actions.setProfileData(response.data.user);
+        actions.setAuthType(response.data.authType);
+        console.log(response.data.authType , "response.data.authType");
+        console.log(state.isLoggedIn);
 
-    checkAuthentication();
-  }, []); // Run this effect only once when the component mounts
+      }
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+    }
+  };
+  
+  
+    useEffect(() => {
+      personalProfile();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [state.isLoggedIn]);; // Run this effect only once when the component mounts
 
   const handleProfileClick = () => {
     setShowProfileDropdown(!showProfileDropdown);
   };
 
   const handleLogout = () => {
-    setIsLoggedIn(false);
+    actions.login(false);
     localStorage.removeItem('token');
     // Additional cleanup if needed
   };
@@ -70,7 +81,7 @@ function Navbar() {
           </ul>
         </div>
         <div>
-          {isLoggedIn ? (
+          {state.isLoggedIn ? (
             <div>
               <Button
                 style={{
@@ -113,7 +124,7 @@ function Navbar() {
           )}
         </div>
       </div>
-      {showSignup && <SignUp show={showSignup} setShow={setShowSignup} setIsLoggedIn={setIsLoggedIn} />}
+      {showSignup && <SignUp show={showSignup} setShow={setShowSignup}  />}
     </div>
 
   );
