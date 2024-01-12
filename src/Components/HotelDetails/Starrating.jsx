03ab5573f2ffb36d486028418c0257af2d2341b2
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2024-01-12 23:56:30 +0530

    Add react-toastify package and update GuestReview component

--- src/Components/HotelDetails/Starrating.jsx
+++ src/Components/HotelDetails/Starrating.jsx
@@ -1,26 +1,24 @@
-import React, {useState} from 'react'
-import { FaStar } from "react-icons/fa6";
+import React from 'react';
+import { FaStar } from 'react-icons/fa6';
 
+function Starrating({ rating, onChange }) {
+  const handleStarClick = (index) => {
+    // Toggle the color of stars on click
+    onChange(index + 1);
+  };
 
-function Starrating() {
-    const [rating, setRating] = useState(4);
-
-    const handleStarClick = (index) => {
-      // Toggle the color of stars on click
-      setRating(index + 1);
-    };
   return (
     <div>
-         {[...Array(5)].map((star, index) => (
+      {[...Array(5)].map((star, index) => (
         <FaStar
           key={index}
           onClick={() => handleStarClick(index)}
           color={index < rating ? '#FDCC0D' : '#ccc'} // Change color based on the selected rating
-          className='inline mx-2 font-700 text-[30px] cursor-pointer'
+          className="inline mx-2 font-700 text-[30px] cursor-pointer"
         />
       ))}
     </div>
-  )
+  );
 }
 
-export default Starrating
+export default Starrating;
--- src/index.js
+++ src/index.js
@@ -5,29 +5,28 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { clientId } from "./config";
-// import { useGoogleLogin } from "@react-oauth/google";
 import { AppProvider } from "./context/store";
-import { FacebookProvider,  } from "react-facebook";
+import { FacebookProvider } from "react-facebook";
 import { FACEBOOK_APP_ID } from "./config";
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <AppProvider>
-    <GoogleOAuthProvider clientId={clientId}>
-    <FacebookProvider appId={FACEBOOK_APP_ID}>
-               
-      <App />
-
-      </FacebookProvider>
-    </GoogleOAuthProvider>
+      <AppProvider>
+        <GoogleOAuthProvider clientId={clientId}>
+          <FacebookProvider appId={FACEBOOK_APP_ID}>
+            <App />
+          </FacebookProvider>
+        </GoogleOAuthProvider>
+        <ToastContainer />
       </AppProvider>
-      </BrowserRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
